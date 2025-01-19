e933ad90e8ad59a5073759a287be07fdcfe5c7c7
Klamkin, Michael <mklamkin3@gatech.edu>
2025-01-18 19:04:51 -0500

    update tests with broken case where optimal solution has no primal variables at bounds

--- test/jump.jl
+++ test/jump.jl
@@ -11,7 +11,7 @@ function simple_model()
     @test termination_status(model) == MOI.OPTIMAL
     @test value(x) == 1.0
     @test value(y) == 0.0
-    @test dual(eq) ≈ 1.0
+    @test isapprox(dual(eq), 1.0, atol=1e-3)
 
     model = Model(SimpleDualAscent.Optimizer)
     @variable(model, 0 ≤ x ≤ 0)
@@ -22,30 +22,30 @@ function simple_model()
     @test termination_status(model) == MOI.OPTIMAL
     @test value(x) == 0.0
     @test value(y) == 1.0
-    @test isapprox(dual(eq), 2.0, atol=2e-3)
+    @test isapprox(dual(eq), 2.0, atol=1e-3)
 
     model = Model(SimpleDualAscent.Optimizer)
-    @variable(model, [0,0][i] ≤ x[i in 1:2] ≤ [1,1][i])
+    @variable(model, [0,-1][i] ≤ x[i in 1:2] ≤ [2,1][i])
     @constraint(model, eq, x[1] + x[2] == 1)
-    @constraint(model, eq2, 2x[1] - x[2] == 2)
+    @constraint(model, ineq, 2x[1] - x[2] ≤ 3)
     @objective(model, Min, x[1] + 2x[2])
     optimize!(model)
-    @test termination_status(model) == MOI.OPTIMAL
-    @test value.(x) == [1.0, 0.0]
-    @test dual(eq) ≈ 0.2
-    @test dual(eq2) ≈ 0.4
+    @test termination_status(model) == MOI.OPTIMAL          broken=true
+    @test isapprox(value.(x), [4/3, -1/3], atol=1e-3)       broken=true
+    @test isapprox(dual(eq), 5/3, atol=1e-3)
+    @test isapprox(dual(ineq), -1/3, atol=1e-3)
 
     model = Model(SimpleDualAscent.Optimizer)
     @variable(model, x ∈ MOI.Interval(0, 1))
     @variable(model, y ∈ MOI.Interval(0, 1))
     @constraint(model, eq, x + y == 1)
-    @constraint(model, ineq, x + 2y ≤ 1)
+    @constraint(model, ineq, x + 2y ≥ 1)
     @objective(model, Min, x + 2y)
     optimize!(model)
     @test termination_status(model) == MOI.OPTIMAL
     @test value(x) == 1.0
     @test value(y) == 0.0
-    @test isapprox(dual(eq), 1.0, atol=2e-3)
+    @test isapprox(dual(eq)+dual(ineq), 1.0, atol=1e-3)
 end
 
 @testset "JuMP" begin
